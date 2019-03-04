afbe24e972d22a740aa3fe6d2db15afa6759df26
leaf715 <shuhan1998@gmail.com>
2019-03-03 19:41:36 -0600

    Adds file

---
+++ recipe_parser.py
@@ -0,0 +1,8 @@
+import urllib2
+
+def main():
+    req = urllib2.Request('https://www.allrecipes.com/recipe/173906/cajun-roasted-pork-loin/?clickId=right%20rail0&internalSource=rr_feed_recipe_sb&referringId=8722%20referringContentType%3Drecipe')
+    pass
+
+if __name__ == "__main__":
+    main()
